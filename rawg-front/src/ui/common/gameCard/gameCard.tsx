f32993c20f8e140a4a11efc2697a76f01a9d53a3
DavidManga <mangadavid2@gmail.com>
2023-09-28 10:51:41 +0300

    set up game card

---
+++ rawg-front/src/ui/common/gameCard/gameCard.tsx
@@ -0,0 +1,99 @@
+import AddIcon from '@mui/icons-material/Add';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import Collapse from '@mui/material/Collapse';
+import { TransitionGroup } from 'react-transition-group';
+import { useState } from 'react';
+import Divider from '@mui/material/Divider';
+
+export function GameCard(props: {
+    // gamename: string;
+    // gameImage: string;
+    // platforms: string[];
+    // releaseDate: string;
+    // genre: string;
+}) {
+    const [display, setDisplay] = useState(false);
+
+    return (
+        <div className="rounded-xl bg-[#282727] mb-4 overflow-hidden">
+            <div className="w-full">
+                <img
+                    className="w-full"
+                    src="https://media.rawg.io/media/crop/600/400/games/fb5/fb5e0fdb1f6bb0e8b5da5d08bb83a5fc.jpg"
+                />
+            </div>
+            <div className="p-2">
+                <div className="mb-2">
+                    <h2 className="fon font-bold sm:text-2xl">
+                        Vampire: The Masquerade - Bloodlines 2
+                    </h2>
+                </div>
+                <div className="mb-2">
+                    <button className="bg-[#414040] sm:text-sm p-1 rounded mr-2">
+                        <span className="text-xs font-bold">
+                            <AddIcon />
+                        </span>
+                        788
+                    </button>
+                    <button className="bg-[#414040] sm:text-sm p-1 rounded mr-2">
+                        <MoreHorizIcon />
+                    </button>
+                </div>
+                <div>
+                    <TransitionGroup>
+                        {display ? (
+                            <Collapse>
+                                <div>
+                                    <div className="flex justify-between mt-3">
+                                        <p className="text-sm text-gray-500">
+                                            Release date
+                                        </p>
+                                        <p className="te text-xs">
+                                            Dec 31, 2023
+                                        </p>
+                                    </div>
+                                    <Divider />
+                                    <div className="flex justify-between mt-3">
+                                        <p className="text-sm text-gray-500">
+                                            Release date
+                                        </p>
+                                        <p className="te text-xs">
+                                            Dec 31, 2023
+                                        </p>
+                                    </div>
+                                    <Divider />
+                                    <div className="flex justify-between mt-3">
+                                        <p className="text-sm text-gray-500">
+                                            Release date
+                                        </p>
+                                        <p className="te text-xs">
+                                            Dec 31, 2023
+                                        </p>
+                                    </div>
+                                    <Divider />
+                                </div>
+                            </Collapse>
+                        ) : null}
+                    </TransitionGroup>
+                </div>
+                <div className="flex justify-center text-sm mb-3">
+                    {display ? (
+                        <p
+                            onClick={() => setDisplay(!display)}
+                            className="x underline underline-offset-8"
+                        >
+                            View less
+                        </p>
+                    ) : (
+                        <p
+                            onClick={() => setDisplay(!display)}
+                            className="x underline underline-offset-8"
+                        >
+                            View more
+                        </p>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+}
--- rawg-front/src/ui/pages/homePage/homepage.tsx
+++ rawg-front/src/ui/pages/homePage/homepage.tsx
@@ -1,24 +1,44 @@
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import Popover from '@mui/material/Popover';
+import Typography from '@mui/material/Typography';
 import { useState } from 'react';
-import Menu from '@mui/icons-material/Menu';
-import { MenuItem } from '@mui/material';
+// import Menu from '@mui/icons-material/Menu';
+// import { MenuItem } from '@mui/material';
+import { GameCard } from '../../common/gameCard/gameCard';
 export function HomePage() {
     const [orderCriteria, setOrderCriteria] = useState('name');
     return (
         <div>
-            <div>
-                <h2 className="sm:text-xl mb-1 font-bold">
-                    New and trending
-                </h2>
-                <p className="sm:text-xs mb-3">
-                    Based on player counts and release date
-                </p>
+            <div className="mb-4">
+                <div>
+                    <h2 className="sm:text-xl mb-1 font-bold">
+                        New and trending
+                    </h2>
+                    <p className="sm:text-xs mb-3">
+                        Based on player counts and release date
+                    </p>
+                </div>
+                <div>
+                    <button className="p-1 text-sm bg-gray-700 text-white sm:mt-2 rounded-md">
+                        Order by{' '}
+                        <span className="font-bold">
+                            {orderCriteria}
+                        </span>
+                        <KeyboardArrowDownIcon />
+                    </button>
+                </div>
             </div>
-            <button className="p-1 text-sm bg-gray-700 text-white sm:mt-2 rounded-md">
-                Order by{' '}
-                <span className="font-bold">{orderCriteria}</span>{' '}
-                <KeyboardArrowDownIcon />
-            </button>
+            <div>
+                <div>
+                    <GameCard />
+                    <GameCard />
+                    <GameCard />
+                    <GameCard />
+                    <GameCard />
+                    <GameCard />
+                    <GameCard />
+                </div>
+            </div>
         </div>
     );
 }
