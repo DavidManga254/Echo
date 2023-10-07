222d100f406450cf22aee226dcb2e80475d52661
DavidManga <mangadavid2@gmail.com>
2023-10-07 21:40:02 +0300

    setup mjml

--- rawg-server/app/Mail/SignUp/SignUpMail.php
+++ rawg-server/app/Mail/SignUp/SignUpMail.php
@@ -4,7 +4,7 @@ namespace App\Mail\SignUp;
 
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Mail\Mailable;
+use Asahasrabuddhe\LaravelMJML\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
@@ -40,7 +40,8 @@ class SignUpMail extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'emails.signUp.signUpEmail',
+            // view: 'emails.signUp.signUpEmail',
+            view: $this->mjml('emails.signUp.signUpEmail')->buildMjmlView()['html'],
         );
     }
 
