d37aff25b6d375027180a7b7e9e5a002563eb959
DavidManga <mangadavid2@gmail.com>
2023-10-24 20:09:24 +0300

    fix alll background image

--- rawg-front/src/ui/pages/loginPage/loginPage.tsx
+++ rawg-front/src/ui/pages/loginPage/loginPage.tsx
@@ -17,7 +17,10 @@ export function LoginPage() {
         })();
     }, []);
     return (
-        <div className="bg-landing-image md:bg-cover md:bg-no-repeat w-screen min-h-screen">
+        <div
+            className="md:bg-cover md:bg-no-repeat w-screen min-h-screen"
+            style={{ backgroundImage: 'url(background.jpg)' }}
+        >
             <div className="w-full min-h-screen bg-black bg-opacity-75 flex items-center text-white justify-center">
                 <div className="sm:w-3/4 md:w-1/4 p-2">
                     <div className=" mb-20">
--- rawg-front/src/ui/pages/signUpPage/signUpPage.tsx
+++ rawg-front/src/ui/pages/signUpPage/signUpPage.tsx
@@ -2,7 +2,10 @@ import { SignUpForm } from '../../common/authForms/signUpForm';
 
 export function SignUpPage() {
     return (
-        <div className="bg-landing-image bg-cover bg-no-repeat w-screen min-h-screen">
+        <div
+            className="bg-cover bg-no-repeat w-screen min-h-screen"
+            style={{ backgroundImage: 'url(background.jpg)' }}
+        >
             <div className="w-full min-h-screen bg-black bg-opacity-75 flex items-center text-white justify-center">
                 <div className="sm:w-3/4 md:w-1/2 lg:w-1/4 p-2">
                     <div className=" mb-10">
